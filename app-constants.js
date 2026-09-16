0a9a125653b7dcd1b16c49a91b1b6e67e3889ba9
gear <gear@local>
2026-09-16 16:18:32 +0800

    chore: bump Wanix runtime to v0.4.46

--- app-constants.js
+++ app-constants.js
@@ -305,9 +305,9 @@ export const BUILTIN_TERMINAL_PROFILES = [
 ];
 
 export const WANIX_RUNTIME = {
-  wasmUrl: "https://w9y.io/go/github.com/justwasm/wanix/wasm@v0.4.44",
+  wasmUrl: "https://w9y.io/go/github.com/justwasm/wanix/wasm@v0.4.46",
   moduleUrl:
-    "https://cdn.jsdelivr.net/gh/justwasm/wanix@v0.4.44/dist/wanix.min.js",
+    "https://cdn.jsdelivr.net/gh/justwasm/wanix@v0.4.46/dist/wanix.min.js",
 };
 
 // The shell toolset (bash/w9y/gear binaries + the rc file) now ships as
